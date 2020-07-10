fca341779f1a86401ae1214c953a3c5e75fa13df
Jil Kothari <jil.kothari@somaiya.edu>
2020-07-10 21:21:23 +0530

    Update run.py

--- app/run.py
+++ app/run.py
@@ -123,3 +123,11 @@ def go():
         query=query,
         classification_result=classification_results
     )
+
+
+def main():	
+    app.run(host='0.0.0.0', port=3001, debug=True)	
+
+
+if __name__ == '__main__':	
+    main()
